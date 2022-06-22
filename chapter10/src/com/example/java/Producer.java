38ebbad03d6a9e81793725512a97fa550bfb69bd
JLopezz3 <lopezjerel@gmail.com>
2022-06-22 16:48:05 -0500

    Producer Consumer

---
+++ chapter10/src/com/example/java/Producer.java
@@ -0,0 +1,46 @@
+package com.example.java;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Producer extends Thread {
+
+    private static final int MAX_SIZE = 3;
+    private final List<String> messages = new ArrayList<>();
+
+    @Override
+    public void run() {
+        try{
+            while(true){
+                produce();
+            }
+        } catch (Exception e){
+
+        }
+    }
+
+    private synchronized void produce() throws Exception {
+        while(messages.size() == MAX_SIZE){
+            System.out.println("Queue limit reached. Waiting for  Consumer");
+            wait();
+            System.out.println("Producer got notification from consumer");
+
+        }
+        String data = LocalDateTime.now().toString();
+        messages.add(data);
+        System.out.println("Producer produced data");
+        notify(); // notify consumer that some data is created
+    }
+
+    public synchronized String consume() throws Exception {
+        notify();
+        while(messages.isEmpty()){
+            wait();
+        }
+        String data = messages.get(0);
+        messages.remove(data);
+        return data;
+    }
+
+}
