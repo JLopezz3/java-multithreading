80e8e75112c710fa432320ffcc84eaa89ce385a6
JLopezz3 <lopezjerel@gmail.com>
2022-06-22 14:06:20 -0500

    Stop Thread Safely

---
+++ Chapter7/src/com/example/java/CustomThread.java
@@ -0,0 +1,28 @@
+package com.example.java;
+
+public class CustomThread extends Thread {
+
+    private boolean shouldExit = false;
+
+    public void setShouldExit(boolean shouldExit) {
+        this.shouldExit = shouldExit;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < 10; i++) {
+                if(shouldExit){
+                    break;
+                }
+
+                System.out.println("Thread is running");
+                Thread.sleep(200);
+            }
+            System.out.println("Thread is completed");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
